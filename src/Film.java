c78a90f1feeb21d96c990162005054844c979521
vacalatyvika02@gmail.com <vacalatyvika02@gmail.com>
2021-09-21 01:32:49 +0300

    Лабораторная работа 1 Вариант 3

---
+++ src/Film.java
@@ -0,0 +1,75 @@
+import java.text.DecimalFormat;
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
+import java.util.Date;
+
+public class Film {
+    private String namefilm;
+    private LocalDateTime dateandtime;
+    private double timeDuration;
+    private String genre;
+    private double budget;
+
+    public Film(String namefilm, LocalDateTime dateandtime, double timeDuration, String genre, double budget) {
+        this.namefilm = namefilm;
+        this.dateandtime = dateandtime;
+        this.timeDuration = timeDuration;
+        this.genre = genre;
+        this.budget = budget;
+    }
+
+    public Film(){}
+    public String getNamefilm() {
+        return namefilm;
+    }
+    public void setNamefilm(String nameFilm) {
+        this.namefilm = namefilm;
+    }
+
+    public LocalDateTime getDateandtime() {
+        return dateandtime;
+    }
+
+    public void setDateandtime(LocalDateTime dateandtime) {
+        this.dateandtime = dateandtime;
+    }
+
+    public double getTimeDuration() {
+        return timeDuration;
+    }
+
+    public double gettimeDuraction() {
+        return timeDuration;
+    }
+    public void setTimeDuration(double timeDuration) {
+        DecimalFormat df = new DecimalFormat("#0.0");
+        String s = df.format(timeDuration);
+        this.timeDuration = s.contains(".6") ? Math.ceil(timeDuration) : timeDuration;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public double getBudget() {
+        return budget;
+    }
+    public void setBudget(double budget) {
+        this.budget = budget;
+    }
+
+    @Override
+    public String toString() {
+        return "Film{" +
+                "nameFilm='" + namefilm + '\'' +
+                ", dateandtime=" + dateandtime +
+                ", timeduraction=" + timeDuration +
+                ", genre=" + genre +
+                ", budget=" + budget +
+                '}';
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,38 @@
+import java.text.ParseException;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
+        Scanner num = new Scanner(System.in);
+        int number = 0;
+        do {
+            System.out.println("Enter the job number :1) easy level 2) average level. If you want to exit output 0.");
+            number = num.nextInt();
+            switch (number) {
+                case 0:
+                    break;
+                case 1:
+                    task1();
+                    break;
+                case 2:
+                    task2();
+                    break;
+                default:
+                    System.out.println("You entered the wrong number(Enter 1-2). ");
+                    break;
+            }
+        } while (number != 0);
 
+    }
+
+    public static void task1() {
         List<Book> books = new ArrayList<>();
         Scanner in = new Scanner(System.in);
 
@@ -15,22 +42,21 @@ public class Main {
         for (int i = 0; i < countOfBook; i++) {
             Book book = new Book();
 
-            System.out.println("Input author of book: ");
+            System.out.println("Enter the author of book: ");
             String authorOfBook = in.next();
             book.setAuthor(authorOfBook);
 
-            System.out.print("Input year of book: ");
+            System.out.println("Enter the year of book: ");
             int yearOfBook = in.nextInt();
             book.setYearPublish(yearOfBook);
 
-            System.out.println("Input tirag of book");
+            System.out.println("Enter the circulation of book");
             int countPageOfPage = in.nextInt();
             book.setCountPage(countPageOfPage);
 
-            System.out.println("Input count of page: ");
+            System.out.println("Enter the count of page: ");
             int tiragOfBook = in.nextInt();
             book.setCountTirag(tiragOfBook);
-
 
             if (tiragOfBook > 150) {
                 books.add(book);
@@ -39,5 +65,60 @@ public class Main {
 
         System.out.println(books.toString());
     }
+
+    public static void task2() throws ParseException {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+//        ZoneId zid = ZoneId.of("Europe/Amsterdam");
+
+        List<Film> films = new ArrayList<>();
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Input count of film: ");
+        int countOfFilm = in.nextInt();
+
+        for (int i = 0; i < countOfFilm; i++) {
+            Film film = new Film();
+
+//            System.out.println("Enter the title of the movie: ");
+//            String nameoffilm = in.next();
+//            film.setNamefilm(nameoffilm);
+
+            System.out.print("Enter the date and time of the session: ");
+            String dateandtime = in.next();
+            film.setDateandtime(LocalDateTime.parse(dateandtime, dtf));
+
+            System.out.println("Enter genre: ");
+            String genre = in.next();
+            film.setGenre(genre);
+            System.out.println("Enter session duration: ");
+            double timeDuration = in.nextDouble();
+            film.setTimeDuration(timeDuration);
+
+            System.out.println("Enter budget:");
+            double budget = in.nextDouble();
+            film.setBudget(budget);
+            films.add(film);
+        }
+
+
+        //  StringBuilder answer = new StringBuilder();
+
+        for (Film film : films) {
+
+            if (film.gettimeDuraction() > 1.4 && film.getDateandtime().getHour() >= 18 && film.getDateandtime().getMinute() > 0) {
+//                String date = String.format("%d/%s/%d %d:%d",film.getDateandtime().getDay(), film.getDateandtime().getMonth(), film.getDateandtime().getYear()
+//                        , film.getDateandtime().getHours(), film.getDateandtime().getMinutes());
+//                answer.append(String.format("\n\n[RESULT]\nName: %s\nSession Date: %s\nDuration: %.1f\nGenres (-s): %s\nBudget: %.1f $\n", date, film.gettimeDuraction(), film.getGenre(), film.getBudget()));
+
+                String data = film.getNamefilm() + " " + film.getDateandtime().getDayOfMonth() + " " + film.getDateandtime().getMonth() + " " + film.getDateandtime().getYear() + " " +
+                        film.getDateandtime().getHour() + " " + film.getDateandtime().getMinute();
+                System.out.println(data);
+            }
+        }
+
+    }
+
+
 }
+
 
